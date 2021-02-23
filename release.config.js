6c7d5553b34c82638195b95bb3c35bfdc8152a28
Michael Crawford <mcrawford@mjcconsulting.com>
2021-02-23 13:13:06 -0800

    fix(ci): Add additional artifacts to build upload

--- release.config.js
+++ release.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  branches: "main",
-  repositoryUrl: "https://github.com/michael-crawford/react-app",
+  branches: 'main',
+  repositoryUrl: 'https://github.com/michael-crawford/react-app',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github'
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { label: 'Build', path: 'build.zip' },
+          { label: 'Coverage', path: 'coverage.zip' }
+        ]
+      }
+    ]
   ]
-}
+};
