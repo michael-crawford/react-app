93a38370d837ee5d65dc94138ce77dc7d6fe4943
Michael Crawford <mcrawford@mjcconsulting.com>
2021-02-24 14:20:12 -0800

    fix(ci): fixed missing re-compile of dist for action

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -604,7 +604,7 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  // throw new Error('Some message')
+  throw new Error('Some message');
   const name = core.getInput('who-to-greet');
   console.log(`Hello ${name}`);
 
