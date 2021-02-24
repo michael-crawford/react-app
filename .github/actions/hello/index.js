7a27ffce70d5d07366af03d5f8f0edbe208afd58
Michael Crawford <mcrawford@mjcconsulting.com>
2021-02-24 14:18:25 -0800

    fix(ci): triggered exception to test try logic

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // throw new Error('Some message')
+  throw new Error('Some message');
   const name = core.getInput('who-to-greet');
   console.log(`Hello ${name}`);
 
