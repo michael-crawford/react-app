3ad0fbec94e2695d2360994c562f3f2ce2355e9b
Michael Crawford <mcrawford@mjcconsulting.com>
2021-02-23 23:29:18 -0800

    fix(ci): fixed configuration related to Husky v5

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Change 1.</p>
+        <p>Change 2.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn
         </a>
       </header>
     </div>
